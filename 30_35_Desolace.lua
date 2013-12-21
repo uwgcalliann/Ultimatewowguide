f28b2dfc473eb09b182cb43dc7c66d08586dd196
Calliann <marc.games@bell.net>
2013-12-20 23:01:27 -0500

    OLD

--- 30_35_Desolace.lua
+++ 30_35_Desolace.lua
@@ -17,7 +17,7 @@ T My Time Has Passed |QID|14184| |N|Cerelia (44.65, 29.49) in Furien's Post| |NP
 A Avenge Furien! |QID|14188| |N|Cerelia (44.65, 29.49) in Furien's Post| |NPC|35295|
 T Avenge Furien! |QID|14188| |N|Sorrem (44.87, 29.55) in Furien's Post| |NPC|35298|
 A Translation |QID|14189| |N|Sorrem (44.87, 29.55) in Furien's Post| |NPC|35298|
-f Furien's Post |N|Grab the Furien's Post Flight Path (44.31, 29.71)| |QID|14191| |NPC|35315| |PL|30|
+f Furien's Post |N|Grab the Furien's Post flight path (44.31, 29.71)| |QID|14191| |NPC|35315| |PL|30|
 N Ride Dragonhawk |N|Speak to Narimar for a ride on the dragonhawk (44.28, 29.70)| |QID|14191| |NPC|35315| |V|
 T Translation |QID|14189| |N|Ancient Tablet (69.19, 29.19)| |OBJ|2652|
 A Furien's Footsteps |QID|14191| |N|Ancient Tablet (69.19, 29.19)| |OBJ|2652|
@@ -25,7 +25,7 @@ A Furien's Footsteps |QID|14191| |N|Ancient Tablet (69.19, 29.19)| |OBJ|2652|
 R Sargeron |N|Travel to Sargeron (76.08, 25.30)| |QID|14219|
 N Visit Elune's Cathedral Ruins |QID|14191.1| |N|Visit Elune's Cathedral Ruins (76.08, 25.30) in Sargeron| |U|47833|
 A To the Hilt! |QID|14219| |N|Elune's Handmaiden (75.95, 26.49) in Sargeron| |OBJ|9053|
-N As you go... |N|Collect 20 [Satyr Sabers] from weapon racks and Hatefury Satyrs and 10 [Satyr Flesh] from Hatefury Satyrs. Tick this step.| |QID|14213| |NPC|4673, 4675, 4670|
+N As you go... |N|Collect 20 [Satyr Sabers] from weapon racks and Hatefury Satyrs and 10 [Satyr Flesh] from Hatefury Satyrs, tick this step| |QID|14213| |NPC|4673, 4675, 4670|
 N Visit Elune's Temple Ruins |QID|14191.2| |N|Visit Elune's Temple Ruins (78.15, 23.86) in Sargeron| |U|47833|
 A Ten Pounds of Flesh |QID|14213| |N|Elune's Brazier (78.66, 23.19) in Sargeron| |OBJ|6661|
 N Visit Elune's Sanctuary Ruins |QID|14191.3| |N|Visit Elune's Sanctuary Ruins (77.32, 18.47) in Sargeron| |U|47833|
@@ -34,9 +34,9 @@ A Nothing a Couple of Melons Won't Fix |QID|14360| |N|Ancient Tablets (77.08, 18
 N 8 [Melonfruit] |N|Gather 8 Melonfruits you can find them on the ground around the waypoint (78.60, 18.70) in Sargeron| |L|48106 8| |QID|14360| |T| |OBJ|434|
 C Nothing a Couple of Melons Won't Fix |QID|14360.1| |N|Use the [Melonfruit] on the Ancient Tablets (76.92, 18.10) in Sargeron| |U|48106| |OBJ|434|
 T Nothing a Couple of Melons Won't Fix |QID|14360| |N|Ancient Tablets (77.00, 18.23) in Sargeron| |OBJ|2652|
-N 20 [Satyr Sabers] |QID|14219| |N|Collect 20 [Satyr Sabers] from the weapon racks and also drops from Hatefury satyrs in the area (76.89, 21.36) in Sargeron| |T| |L|48943 20| |NPC|4673, 4675, 4670|
-K Hellfire Satry |QID|14213| |N|Collect 10 pieces of [Satyr Flesh] from any Hatefury satyrs in the area, then place them on Elune's Brazier (76.89, 21.36) in Sargeron| |L|48857 10| |NPC|4673, 4675, 4670|
-C To the Hilt! |QID|14219| |N|Use the [Satyr Sabers] on Elune's Handmaiden to complete Elune's Ritual of War (75.96, 26.53)| |U|48943| |OBJ|9053|
+N 20 [Satyr Sabers] |QID|14219| |N|Collect 20 [Satyr Sabers] from the weapon racks and also drops from Hatefury satrys in the area (76.89, 21.36) in Sargeron| |T| |L|48943 20| |NPC|4673, 4675, 4670|
+K Hellfire Satry |QID|14213| |N|Collect 10 pieces of [Satyr Flesh] from any Hatefury satyrs in the area. Then place them on Elune's Brazier (76.89, 21.36) in Sargeron| |L|48857 10| |NPC|4673, 4675, 4670|
+C To the Hilt! |QID|14219| |N|Use the [Satyr Sabers] on Elune's Handmaiden to complete Elune's Ritual of War (75.96, 26.53)| |U|181627| |OBJ|9053|
 T To the Hilt! |QID|14219| |N|Elune's Handmaiden (75.94, 26.54) in Sargeron| |OBJ|9053|
 C Ten Pounds of Flesh |QID|14213| |N|Use the [Satyr Flesh] on Elune's Brazier (78.68, 23.12) in Sargeron| |U|48857| |OBJ|6661|
 T Ten Pounds of Flesh |QID|14213| |N|Elune's Brazier (78.68, 23.15) in Sargeron| |OBJ|6661|
@@ -49,7 +49,6 @@ C Firestarter |QID|14196.1| |N|Use the [Vortex Gem] to summon a Wispy Vortex, th
 T Firestarter |QID|14196| |N|Ancient Vortex Runestone (75.91, 18.33) in Sargeron| |OBJ|235|
 A Rider on the Storm |QID|14198| |N|Ancient Vortex Runestone (75.91, 18.33) in Sargeron| |OBJ|235|
 C Rider on the Storm |QID|14198.1| |N|Use the [Raging Vortex Gem] at Nijel's Point Ruin (67.76, 17.32) and kill 40 Nijel's Point Defenders and 15 Nijel's Point Merchants| |U|48249| |NPC|35506, 35510|
-C Rider on the Storm |QID|14198.2| |N|Use the [Raging Vortex Gem] at Nijel's Point Ruin (67.76, 17.32) and kill 40 Nijel's Point Defenders and 15 Nijel's Point Merchants| |U|48249| |NPC|35506, 35510|
 
 R Furien's Post |N|Travel to Furien's Post (44.66, 29.47)| |QID|14255|
 T Rider on the Storm |QID|14198| |N|Cerelia (44.66, 29.47) in Furien's Post| |NPC|35295|
@@ -57,7 +56,7 @@ A Ethel Rethor |QID|14255| |N|Cerelia (44.66, 29.47) in Furien's Post| |NPC|3529
 A Peace of Mind |QID|14223| |N|Sorrem (44.84, 29.52) in Furien's Post| |NPC|35298|
 
 R Ethel Rethor |N|Travel to Ethel Rethor (39.00, 27.00)| |QID|14256|
-f Ethel Rethor |N|Grab the Ethel Rethor Flight Path (39.00, 27.00)| |QID|14256| |NPC|35562| |PL|30|
+f Ethel Rethor |N|Grab the Ethel Rethor flight path (39.00, 27.00)| |QID|14256| |NPC|35562| |PL|30|
 T Ethel Rethor |QID|14255| |N|Cenarion Researcher Korrah (38.82, 27.03) in Ethel Rethor| |NPC|35773|
 A The Emerging Threat |QID|14256| |N|Cenarion Researcher Korrah (38.82, 27.03) in Ethel Rethor| |NPC|35773|
 C The Emerging Threat |QID|14256.1| |N|Speak to Cenarion Researcher Korrah for a ride (38.82, 27.03) in Ethel Rethor| |NPC|35773|
@@ -100,7 +99,7 @@ A You'll Know It When You See It |QID|14225| |N|Auto quest| |NPC|35298| |PPOS|
 C You'll Know It When You See It |QID|14225.1| |N|Collect the [Ancient Summoning Ritual] from the chest in the western-most building (52.88, 29.04), there is a 5-10 min respawn time so kill cultist while you wait. Thunder Axe Fortress| |OBJ|5743|
 T You'll Know It When You See It |QID|14225| |N|Sorrem (52.85, 29.11) in Thunder Axe Fortress| |NPC|35298|
 A Putting Their Heads Together |QID|14227| |N|Sorrem (52.85, 29.13) in Thunder Axe Fortress| |NPC|35298|
-N 100 [Bleached Skulls] |QID|14227| |N|Collect 100 [Bleached Skulls] from the piles of skulls in Thunder Axe Fortress. Work your way towards the summoning circle at the waypoint. (54.75, 26.45)| |OBJ|293| |L|48953 100|
+N 100 [Bleached Skulls] |QID|14227| |N|Collect 100 [Bleached Skulls] from the piles of skulls in Thunder Axe Fortress, work your way towards the summoning circle at the waypoint. (54.75, 26.45)| |OBJ|293| |L|48953 100|
 C Putting Their Heads Together |QID|14227.1| |N|Use the [Bleached Skulls] at the Burning Blade Summoning Circle, then kill Nazargen (54.94, 26.66) in Thunder Axe Fortress| |NPC|35647| |U|48953|
 C Ears Are Burning |QID|14232| |N|Collect 25 [Burning Blade Ears] from the burning blade cultists (54.56, 27.06) in Thunder Axe Fortress| |NPC|4664, 4665, 4663|
 
@@ -109,7 +108,7 @@ A Good Gold For Bad Tail |QID|14252| |N|Bizby (60.96, 28.94)| |NPC|35757|
 A Fletch Me Some Plumage! |QID|14253| |N|Bizby (60.96, 28.86)| |NPC|35757|
 
 R Thunk's Abode |N|Travel to Thunk's Abode (70.35, 32.97)| |QID|14247|
-f Thunk's Abode |QID|14247| |N|Grab Thunk's Abode Flight Path from Thunk's Wyvern (70.65, 32.89)| |NPC|35556|
+f Thunk's Abode |QID|14247| |N|Grab Thunk's Abode flight path from Thunk's Wyvern (70.65, 32.89)| |NPC|35556|
 A Early Adoption |QID|14246| |N|Cenarion Embassador Thunk (70.35, 32.97) in Thunk's Abode| |NPC|35661|
 A Stubborn Lands |QID|14247| |N|Cenarion Embassador Thunk (70.35, 32.97) in Thunk's Abode| |NPC|35661|
 
@@ -118,7 +117,7 @@ T Putting Their Heads Together |QID|14227| |N|Sorrem (44.87, 29.54) in Furien's 
 T Ears Are Burning |QID|14232| |N|Sorrem (44.87, 29.54) in Furien's Post| |NPC|35298|
 
 R Thunk's Abode |N|Travel to Thunk's Abode (70.35, 32.97)| |QID|14254|
-N As you go... |N|Kill Gritjaw Basilisks (crocs), Scorpashi Snappers (scorpions) and Dread Swoops (vultures) for the next 3 quests. Tick this step.| |QID|14253| |NPC|4728, 4696, 4692|
+N As you go... |N|Kill Gritjaw Basilisks (crocs), Scorpashi Snappers (scorpions) and Dread Swoops (vultures) for the next 3 quests, tick this step| |QID|14253| |NPC|4728, 4696, 4692|
 T Stubborn Lands |QID|14247| |N|Smouldering Stone (69.31, 21.35)| |NPC|195600|
 A Heavy Metal |QID|14254| |N|Smouldering Stone (69.31, 21.35)| |NPC|195600|
 C Early Adoption |QID|14246| |N|Place 3 Swoop Eggs in Swoop Nests and protect them until they are collected (67.40, 22.25)| |U|49014| |OBJ|7143|
@@ -127,7 +126,7 @@ C Good Gold For Bad Tail |QID|14252.1| |N|Collect 15 [Aged Basilisk Tails] from 
 C New Beginnings |QID|14251.1| |N|Collect 15 [Scorpion Stingers] from Scorpashi Snappers (65.26, 25.08)| |NPC|4696|
 C Fletch Me Some Plumage! |QID|14253.1| |N|Collect 25 [Dread Swoop Feathers] from Dread Swoops (60.15, 23.82)| |NPC|4692|
 
-R Thunk's Abode |QID|14246| |N|Travel to Thunk's Abode (70.35, 32.97)|
+R Thunk's Abode |N|Travel to Thunk's Abode (70.35, 32.97)|
 T Early Adoption |QID|14246| |N|Cenarion Embassador Thunk (70.35, 32.97) in Thunk's Abode| |NPC|35661|
 
 T Heavy Metal |QID|14254| |N|Bizby (61.01, 28.98)| |NPC|35757|
@@ -139,7 +138,7 @@ R Kormek's Hut |N|Travel to Kormek's Hut (62.36, 39.17)| |QID|5501|
 A Bone Collector |QID|5501| |N|Bibbly F'utzbuckle (62.36, 39.17) in Kormek's Hut| |NPC|11438|
 
 R Karnum's Glade |N|Travel to Karnum's Glade (57.70, 49.70)| |QID|14305|
-f Karnum's Glade |N|Grab the Karnum's Glade Flight Path (57.70, 49.70)| |QID|14305| |NPC|35478| |PL|30|
+f Karnum's Glade |N|Grab the Karnum's Glade flight path (57.70, 49.70)| |QID|14305| |NPC|35478| |PL|30|
 h Karnum's Glade |N|Set Hearth to Karnum's Glade (56.74, 49.99)| |QID|14305| |NPC|43872|
 A A Time to Reap |QID|14305| |N|Botanist Ferrah (58.55, 48.65) in Karnum's Glade| |NPC|36060|
 A Pulling Weeds |QID|14306| |N|Botanist Ferrah (58.55, 48.65) in Karnum's Glade| |NPC|36060|
@@ -166,7 +165,8 @@ R Kodo Graveyard |N|Travel to Kodo Graveyard (52.22, 53.55)| |QID|14343|
 C Delivery Device |QID|14339.1| |N|Collect 3 [Hollow Kodo Horns] from Dying Kodos (48.67, 61.08) in Kodo Graveyard| |NPC|4701|
 C Bone Collector |QID|5501| |N|Collect 10 [Kodo Bones] from the Kodo Graveyard (51.11, 59.87) in Kodo Graveyard| |OBJ|4453|
 C A Time to Reap |QID|14305.1| |N|Collect 10 [Aloe Thistles] (55.82, 63.81) in Kodo Graveyard| |OBJ|9079|
-C Stubborn Winds |QID|14307.1| |N|Destroy 5 Whirlwind Stormwalkers. (45.97, 51.83) | |NPC|11577|
+R Kodo Graveyard |N|Travel to Kodo Graveyard (52.22, 53.55)| |QID|14307|
+C Stubborn Winds |QID|14307.1| |N|Destroy 5 Whirlwind Stormwalkers (45.97, 51.83) | |NPC|11577|
 T Delivery Device |QID|14339| |N|Maurin Bonesplitter (52.22, 53.55) in Ghost Walker Post| |NPC|4498|
 A Maurin's Concoction |QID|14343| |N|Maurin Bonesplitter (52.22, 53.55) in Ghost Walker Post| |NPC|4498|
 
@@ -227,20 +227,20 @@ C Jugkar's Undoing |QID|14344.1| |N|Use the [Infernal Power Core] to take contro
 T Jugkar's Undoing |QID|14344| |N|Felgur Twocuts (54.05, 76.98) in Mannoroc Coven| |NPC|5395|
 
 R Gelkis Village |N|Travel to Gelkis Village (42.23, 83.30)| |QID|14329|
-K Sherik |QID|14328.2| |N|Kill Sherik (35.62, 81.36) in Gelkis Village| |NPC|36182|
+K Sherik |QID|14328.2| |N|Kill Sherik (36.15, 80.90) in Gelkis Village| |NPC|36182|
 K Rhoho |QID|14328.1| |N|Kill Rhoho (40.89, 82.15) in Gelkis Village| |NPC|36181|
 K Jorreth |QID|14328.3| |N|Kill Jorreth (35.66, 91.26) in Gelkis Village| |NPC|36183|
 
-R Bolgan's Hole |N|Travel to Bolgan's Hole (37.75, 85.37) (38.83, 92.05) (40.45, 95.52)| |QID|14328|
+R Bolgan's Hole |N|Travel to Bolgan's Hole (38.83, 92.05) (40.45, 95.52)| |QID|14329|
 T Three Princes |QID|14328| |N|Khan Shodo (38.83, 92.05) (40.45, 95.52) in Bolgan's Hole| |NPC|36185|
 A Not So Fast! |QID|14329| |N|Khan Shodo (40.45, 95.52) in Bolgan's Hole| |NPC|36185|
 
-R Shadowprey Village |N|Travel to Shadowprey Village (36.31, 76.73) (22.70, 72.07)| |QID|14334|
+R Shadowprey Village |N|Travel to Shadowprey Village (39.8, 78.3) (22.70, 72.07)| |QID|14334|
 T Shadowprey Village |QID|14337| |N|Mai'Lahii (22.70, 72.07) in Shadowprey Village| |NPC|12031|
 A Blubbergut |QID|14334| |N|Mai'Lahii (22.70, 72.07) in Shadowprey Village| |NPC|12031|
 A Fish in a Bucket |QID|5421| |N|Jinar'Zillen (22.49, 73.04) in Shadowprey Village| |NPC|11317|
-f Shadowprey Village |N|Grab the Shadowprey Village Flight Path (21.57, 74.03)| |QID|14335| |NPC|6726| |PL|30|
-C Blubbergut |QID|14334.1| |N|Kill Blubbergut. He patrols a complete loop that takes him close to the dock in Shadowprey Village (17.97, 75.42) The Veiled Sea| |NPC|36206|
+f Shadowprey Village |N|Grab the Shadowprey Village flight path (21.57, 74.03)| |QID|14335| |NPC|6726| |PL|30|
+C Blubbergut |QID|14334.1| |N|Kill Blubbergut, he patrols a complete loop that takes him close to the dock in Shadowprey Village (17.97, 75.42) The Veiled Sea| |NPC|36206|
 C Fish in a Bucket |QID|5421.1| |N|Collect 5 [Shellfish] from the traps around the end of the dock (20.63, 75.01) in Sar'theris Strand| |OBJ|1787|
 T Blubbergut |QID|14334| |N|Mai'Lahii (22.68, 72.03) in Shadowprey Village| |NPC|12031|
 T Fish in a Bucket |QID|5421| |N|Jinar'Zillen (22.49, 73.05) in Shadowprey Village| |NPC|11317|
@@ -248,12 +248,12 @@ A Chipping In |QID|14335| |N|Taiga Wisemane (25.73, 68.20) in Shadowprey Village
 C Chipping In |QID|14335| |N|Kill 5 Drysnap Pincers and 5 Drysnap Crawlers (25.54, 81.90) in Sar'theris Strand| |NPC|11563, 11562| 
 T Chipping In |QID|14335| |N|Taiga Wisemane (25.73, 68.14) in Shadowprey Village| |NPC|11624|
 
-R Valley of Spears |N|Travel to Valley of Spears (27.60, 75.79) (30.16, 73.55) (33.86, 53.43)| |QID|6132|
+R Valley of Spears |N|Travel to Valley of Spears (33.86, 53.43)| |QID|6132|
 A Get Me Out of Here! |QID|6132| |N|Melizza Brimbuzzle (33.86, 53.43) in Valley of Spears| |NPC|12277|
 C Get Me Out of Here! |QID|6132.1| |N|Escort Melizza Brimbuzzle (39.91, 61.38) in Valley of Spears| |NPC|12277|
 C Not So Fast! |QID|14329.1| |N|Kill 10 Maraudine Maulers (30.19, 57.35) in Valley of Spears| |NPC|4656|
-K Maraudine Maulers |N|Kill Maraudine Maulers until you find the [Maraudine Prisoner Manifest] (30.19, 57.35) in Valley of Spears| |L|38567| |QID|14330| |NPC|4656|
-A Behind Closed Doors |QID|14330| |N|Use the [Maraudine Prisoner Manifest] to accept this quest. Drops from Maraudine Maulers (30.19, 57.35) in Valley of Spears| |U|38567|
+K Maraudine Maulers |N|Kill Maraudine Maulers until you find [Maraudine Prisoner Manifest] (30.19, 57.35) in Valley of Spears| |L|38567| |QID|14330| |NPC|4656|
+A Behind Closed Doors |QID|14330| |N|Use [Maraudine Prisoner Manifest] to accept this quest. Drops from Maraudine Maulers (30.19, 57.35) in Valley of Spears| |U|38567|
 
 R Maraudon |N|Travel to Maraudon (29.30, 62.55)| |QID|14333|
 A While You're Here |QID|14333| |N|Broken Relic (28.3, 64) (27.2, 63.4) in Maraudon| |OBJ|406|
@@ -261,8 +261,7 @@ T Behind Closed Doors |QID|14330| |N|Kherrah (27.3, 63) in Maraudon| |NPC|36196|
 A Time For Change |QID|14332| |N|Kherrah (27.3, 63) in Maraudon| |NPC|36196|
 
 R Valley of Spears |N|Travel to Valley of Spears (28.3, 64) (29.30, 62.55) (30.07, 58.02)| |QID|14333|
-N [Defiled Relics] |QID|14333| |N||N|Find 6 [Defiled Relics] found scattered throughout the Maraudine camp (30.07, 58.02) in Valley of Spears| |L|49194 6| |OBJ|406|
-C While You're Here |QID|14333.1| |N|Use the [Defiled Relics] to destroy them| |U|49194| |OBJ|406|
+C While You're Here |QID|14333.1| |N|Find 6 [Defiled Relics] found scattered throughout the Maraudine camp (30.07, 58.02) in Valley of Spears| |OBJ|406|
 T While You're Here |QID|14333| |N|Khan Shodo (30.07, 58.02) in Valley of Spears| |NPC|36185|
 T Not So Fast! |QID|14329| |N|Khan Shodo (30.16, 57.48) in Valley of Spears| |NPC|36185|
 
@@ -278,7 +277,7 @@ R Shok'Thokar |N|Travel to Shok'Thokar (72.2, 67.4)| |QID|14393|
 T Time For Change |QID|14332| |N|Khan Leh'Prah (72.2, 67.4) in Shok'Thokar| |NPC|36398|
 A Into the Fray! |QID|14393| |N|Khan Leh'Prah (72.2, 67.4) in Shok'Thokar| |NPC|36398|
 K Priestess Than'darissa |QID|14393.3| |N|Kill Priestess Than'darissa. She moves around, so you'll probably have to look for her (70, 75) in Shok'Thokar| |NPC|36413|
-C Into the Fray! |N|Kill 15 Burning Blade Warlocks and 3 Doomguard Invaders (74.7, 74.9) in Shok'Thokar| |QID|14393| |NPC|36414, 36412|
+C Into the Fray! |N|Kill 15 Burning Blade Warlocks and 3 Doomguard Invaders (74.7, 74.9) in Shok'Thokar| |QID|14394| |NPC|36414, 36412|
 T Into the Fray! |QID|14393| |N|Khan Leh'Prah (72.2, 67.4) in Shok'Thokar| |NPC|36398|
 A Death to Agogridon |QID|14394| |N|Khan Leh'Prah (72.2, 67.4) in Shok'Thokar| |NPC|36398|
 C Death to Agogridon |QID|14394| |N|Defeat Agogridon (76.70, 75.93) (79.77, 79.21) (81.8, 80.4) in Shok'Thokar| |NPC|36442|
